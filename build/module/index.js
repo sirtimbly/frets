3fbd6c83a791c62700c2123fed3a12ee63e11310
Tim Bendt <sirtimbly@gmail.com>
2020-02-07 19:08:12 -0700

    fix: lint rename and build config

--- build/module/index.js
+++
@@ -1,5 +0,0 @@
-import * as maquette from 'maquette';
-export { setup } from './frets';
-export { PropsWithFields } from './props-field-registry';
-export { maquette };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxLQUFLLFFBQVEsTUFBTSxVQUFVLENBQUM7QUFHckMsT0FBTyxFQUNOLEtBQUssRUFRTCxNQUFNLFNBQVMsQ0FBQztBQUNqQixPQUFPLEVBQUMsZUFBZSxFQUFDLE1BQU0sd0JBQXdCLENBQUM7QUFDdkQsT0FBTyxFQUFDLFFBQVEsRUFBQyxDQUFDIn0=
--- build/module/props-field-registry.js
+++
@@ -1,9 +0,0 @@
-export class PropsWithFields {
-    constructor(data) {
-        this.registeredFieldsValues = {};
-        this.registeredFieldsState = {};
-        this.registeredFieldValidationErrors = {};
-        Object.assign(this, data);
-    }
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJvcHMtZmllbGQtcmVnaXN0cnkuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvcHJvcHMtZmllbGQtcmVnaXN0cnkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxPQUFPLGVBQWU7SUFhM0IsWUFBWSxJQUFVO1FBWmYsMkJBQXNCLEdBRXpCLEVBQUUsQ0FBQztRQUVBLDBCQUFxQixHQUV4QixFQUFFLENBQUM7UUFFQSxvQ0FBK0IsR0FFbEMsRUFBRSxDQUFDO1FBR04sTUFBTSxDQUFDLE1BQU0sQ0FBQyxJQUFJLEVBQUUsSUFBSSxDQUFDLENBQUM7SUFDM0IsQ0FBQztDQUNEIn0=
